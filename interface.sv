eff14a0ee1bd7c97b4787c8fddc423bfd49ea214
Omar Mohammed Badr <105713653+OmarBadr108@users.noreply.github.com>
2024-08-13 18:08:45 +0300

    Delete interface.sv

--- interface.sv
+++
@@ -1,9 +0,0 @@
-interface alu_interface (input logic clock);
-
-	logic       reset ;
-	logic [7:0] A , B ;
-	logic [3:0] op_code ;
-	logic [7:0] result ;
-	bit 	    CarryOut ;
-
-endinterface : alu_interface
